bac17fa43920a794c3c0853f1aa6c4583307fcea
Dan <daniel@rpimentel.com>
2020-10-01 15:17:45 -0400

    getting rid of default and creating comps using Bu

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+<section class="section">
+    <div class="container">
+      <Nav />
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+       <router-view/>
     </div>
-    <router-view/>
-    Welcome to New Paltz
-  </div>
+  </section>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Nav from "@/components/Nav";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+    components:{
+      Nav
     }
-  }
 }
+</script>
+
+<style lang="scss">
+
 </style>
