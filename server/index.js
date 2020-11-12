f04d7bd7bb07e5156175dea14f79a771aa618ae9
Dan <daniel@rpimentel.com>
2020-11-12 10:48:39 -0500

    catch all to enable deep linking

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const users = require('./controllers/users');
@@ -8,8 +9,13 @@ const port = process.env.PORT || 3000;
 
 console.log(process.env.REST_CLASS);
 
-app.use(express.json());
 
+//middle ware
+app.use(express.json());
+app.use(express.static(__dirname + '/../docs/'))
+
+
+//authentication
 app.use(function(req,res,next){
     const arr = (req.headers.authorization || "").split(" ");
     if(arr.length > 1 && arr[1]!=null){
@@ -18,17 +24,25 @@ app.use(function(req,res,next){
     next();
 });
 
-app.get('/',(req,res) => {
+
+//API
+app.get('/hello',(req,res) => {
     res.send('Hello Hudson Valley! You requested '+ req.url)
 })
 
 app.use('/users',users);
+
+app.get('*',(req,res,next)=>{
+    res.sendFile( path.join(__dirname + '/../docs/index.html') )
+})
 
 app.use( (err, req, res, next) =>{
     console.log(err);
     res.status(err.status || 500).send( { message: err.message } )
 } )
 
+
+//init
 app.listen(port,() => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
